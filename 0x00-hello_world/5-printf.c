ed18c3fb5602286a2b516f420ace21819509c9af
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-03-20 16:35:20 -0700

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function will print phrase in argument
+ *
+ * Return: will be 0 when succesfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
