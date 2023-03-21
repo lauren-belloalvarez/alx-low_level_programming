6379311ea606e742c9866d47287285d49cc61058
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-03-20 18:13:17 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: will be 0 for succesfull output 
+ */
+
+int main(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
