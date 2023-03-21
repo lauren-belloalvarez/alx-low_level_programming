0482637539f29082757260cc3b1c771020f8d8e2
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-03-20 19:00:20 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 16 in lowercase
+ *
+ * Return: will be 0 if successfull
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
