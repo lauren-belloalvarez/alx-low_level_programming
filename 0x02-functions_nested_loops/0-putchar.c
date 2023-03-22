cd64bd1bd2b9bd02ac45139492ae1cc77041fcdd
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-03-22 14:06:23 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints input putchar
+ * _putchar: prints value
+ *
+ * Return: will be 0 if succesfull
+ *
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
