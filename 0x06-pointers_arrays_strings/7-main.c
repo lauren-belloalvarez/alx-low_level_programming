9e8f793926bf8fb74f5b0e819f84d179e0ad29e8
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-03-30 00:41:54 -0700

    Task7

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
  * main - check the code for
  *
@@ -5,11 +8,11 @@
  */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-	char *p;
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
 
-	p = leet(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
