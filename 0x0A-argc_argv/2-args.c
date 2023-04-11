23d967117dd864e9b87d5780e46ea82bc74177f8
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-04-11 13:56:56 -0700

    Task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints out all arguments
+ *
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b = 0;
+
+	if (argc > 1)
+	{
+		for (a = 0; a < argc; a++)
+		{
+			printf("%s\n", argv[a]);
+			b = b + atoi(argv[a]);
+		}
+	}
+	return (0);
+}
