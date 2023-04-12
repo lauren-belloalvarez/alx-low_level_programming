f2fb9e0bd03fca0506b06de17e2f39c5d415c2e0
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-04-12 09:54:46 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int b;
 
-	a = malloc(size * sizeof(char));
+	a = malloc(sizeof(char) * size);
 	if (size == 0)
 	{
 		return ("NULL");
