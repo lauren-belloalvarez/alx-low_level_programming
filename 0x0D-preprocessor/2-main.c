0c6aa4e4f724d692d5bc48d94b071f7587443d2d
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-04-17 13:55:40 -0700

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - returns name of current file
+ * 
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return(0);
+}
