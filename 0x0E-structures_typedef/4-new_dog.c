28e0ab69f9137b82927fc040fc156ab2f08b024d
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-04-17 16:15:06 -0700

    task4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - details of new dog
+ *
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ *
+ * Return: NULL on failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
