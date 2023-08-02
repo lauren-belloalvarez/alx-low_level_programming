43aec7c683213a4ee70250e5df33714c0776d1ec
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-08-02 14:51:15 -0700

    Task 0 - Print a String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string followed by a new line
+ *
+ * @s: string that will be used as the input
+ *
+ * Return: 0 when printed successfully
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+0x08.C-Recursion Tasks 0-6 and Advanced Tasks
