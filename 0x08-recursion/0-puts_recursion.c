1a0995bdeb891202e22f9001b4506d753cbee47d
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-08-02 15:12:11 -0700

    Task 0 - print a string corrected

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - prints string followed by a new line
  *
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,10 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
