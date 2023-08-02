3c09b0e1d3169ace1cc04c16e497334d6c0f7ed8
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-08-02 16:54:58 -0700

    task 5 - square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * root - finds natural square root of number
+ *
+ * @n: input number
+ * @counter: keeps track of iterations
+ *
+ * Return: square root result
+ */
+
+int root(int n, int counter)
+{
+	if (n < 0 || counter * counter > n)
+	{
+		return(-1);
+	}
+	else if (counter * counter == n)
+	{
+		return(counter);
+	}
+	return(root(n, counter + 1));
+}
+
+/**
+ * _int_sqrt_recursion - calculates square root of number
+ *
+ * @n: input number
+ *
+ * Return: result of the squared root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return(root(n, 0));
+}
