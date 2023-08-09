376078d081a35bb15d9b5d425194a24e2b14d33e
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-08-09 04:56:23 -0700

    Task 0-Programme that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints name of the programme
+ *
+ * argc: number of inputs
+ * argv: array of inputs
+ *
+ * Return: function name
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
