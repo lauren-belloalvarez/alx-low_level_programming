3f59506885107da8dbf4455ba66711d9de2175fe
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-08-10 14:17:28 -0700

    Task 5 Multiplying 2 positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memset(char *s, char b, unsigned int n);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int main(int argc, char *argv[]);
 
 #endif
